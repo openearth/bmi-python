97f08e898e24a8c0777eb003d46e069114370f41
Fedor Baart <fedor.baart@deltares.nl>
2017-06-28 15:17:17 +0200

    version update

--- bmi/__init__.py
+++ bmi/__init__.py
@@ -2,6 +2,6 @@ import sys
 import pkg_resources
 
 if hasattr(sys, "frozen"):
-    __version__ = '0.2'
+    __version__ = '0.2.1'
 else:
     __version__ = pkg_resources.get_distribution("bmi").version
