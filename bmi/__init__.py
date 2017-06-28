286d55736002d3db3755ede14420e6d65f43bacc
Fedor Baart <fedor.baart@deltares.nl>
2017-06-28 15:17:36 +0200

    Bump version: 0.2.1 → 0.2.2

--- bmi/__init__.py
+++ bmi/__init__.py
@@ -2,6 +2,6 @@ import sys
 import pkg_resources
 
 if hasattr(sys, "frozen"):
-    __version__ = '0.2.1'
+    __version__ = '0.2.2'
 else:
     __version__ = pkg_resources.get_distribution("bmi").version
